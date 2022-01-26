1a5e91ff85e6fff49bad3fd6d403b600a3cbe9b5
Charles Torre <torre_charles@hotmail.com>
2022-01-25 19:33:24 -0800

    Update Program.cs

--- GuanExamples/Program.cs
+++ GuanExamples/Program.cs
@@ -73,7 +73,7 @@ namespace GuanExamples
                 // time() is a system predicate. Used without an arg, it results in DateTime.UtcNow. With a TimeSpan arg, it results in DateTime.UtcNow + arg.
                 // Guan will automatically convert a TimeSpan representation (like 1.00:00:00) to a .NET TimeSpan object.
                 "testdate(?dt) :- time(-1.00:00:00) > ?dt, WriteInfo('time() impl: {0} is more than 1 day ago', ?dt)",
-                // Or, for no good reason other than demonstrating how to write an external predicate that binds a value to rule variable (see GetDateTimeUtcNowPredicateType.cs).
+                // Or, for no good reason other than demonstrating how to write an external predicate that binds a value to a rule variable (see GetDateTimeUtcNowPredicateType.cs).
                 // You could use any value for the variable name used in utcnow. The key is that the predicate takes a single argument: a variable (of Guan type IndexedVariable, in fact).
                 "testdate1(?dt) :- utcnow(?n), ?n - ?dt > 1.00:00:00, WriteInfo('utcnow impl: {0} is more than 1 day ago', ?dt)",
                 // This will throw a GuanException because the argument for utcnow is grounded, not a variable.
