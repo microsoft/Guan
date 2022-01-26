e05f20a53d1ed68f885ee0d63b7430c847d61850
Charles Torre <ctorre@microsoft.com>
2022-01-25 19:39:39 -0800

    Lic

--- GuanExamples/ExternalPredicates/TestDivPredicateType.cs
+++ GuanExamples/ExternalPredicates/TestDivPredicateType.cs
@@ -1,4 +1,9 @@
-﻿using Guan.Logic;
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using Guan.Logic;
 using System;
 using System.Threading.Tasks;
 
--- GuanExamples/Program.cs
+++ GuanExamples/Program.cs
@@ -1,4 +1,9 @@
-﻿using Guan.Logic;
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using Guan.Logic;
 using GuanExamples.ExternalPredicates;
 using System.Collections.Generic;
 using System.Threading.Tasks;
